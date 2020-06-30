4649564604aa97ad50fab52338809090e83a1926
Kallio Mikko <mikko.kallio@elisa.fi>
2020-06-30 06:18:45 +0300

    Restructure repo

---
+++ build/precache-manifest.f48f550e851245304be4706d78999c7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86ad93c7bca41060b469b082b1c81a8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91211e4a433ee8ba77cf",
+    "url": "/static/css/main.efc48fce.chunk.css"
+  },
+  {
+    "revision": "d72cc7c3f59543947486",
+    "url": "/static/js/2.029bf66b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.029bf66b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91211e4a433ee8ba77cf",
+    "url": "/static/js/main.e76d6e60.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
---
+++ index.js
@@ -0,0 +1,126 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) });
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(cors())
+app.use(express.static('build'))
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4
+  }
+]
+
+
+app.get('/', (req, res) => {
+  res.send('<h1>Try /api/notes</h1>')
+})
+
+app.get('/info', (req, res) => {
+  const info = persons.length
+  const time = new Date()
+  res.send(`
+    <p>Phonebook has info for ${info} people</p>
+    <p>${time}</p>
+  `)
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+const generateId = () => {
+  let id
+  while (true) {
+    id = Math.floor(Math.random() * 100000)
+    if (persons.filter(person => person.id === id).length === 0) return id
+  }
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'name or number missing'
+    })
+  }
+
+  if (persons.map(person => person.name).includes(body.name)) {
+    return response.status(400).json({
+      error: 'name already exists'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
